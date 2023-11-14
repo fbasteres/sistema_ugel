c2d3376f60c7e91382135690a18bf289d280533a
fbasteres <ferjav4351@gmail.com>
2023-11-14 12:59:33 -0500

    dataTable-idiomas

--- resources/views/layouts/partials/datatable.blade.php
+++ resources/views/layouts/partials/datatable.blade.php
@@ -1,6 +1,10 @@
 <script>
     $('#tabla_usuario, #ejemplo').DataTable( {
+        scrollX: true,
         language: {
+            emptyTable:     "No hay datos disponibles en la tabla",
+            info:           "Mostrando _START_ al _END_ de _TOTAL_ entradas",
+            lengthMenu:     "Filtrar _MENU_ entradas",
             search: "Buscar:",
             paginate: {
             previous:   "Anterior",
